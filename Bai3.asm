92903ba146a964782f043c3b8e12a9c51e05b7da
VODINHTHANH <58946497+VODINHTHANH@users.noreply.github.com>
2020-11-08 16:01:39 +0700

    Update Bai3.asm

--- Bai3.asm
+++ Bai3.asm
@@ -2,9 +2,9 @@
     n1: .asciiz "Enter a: "
     n2: .asciiz "Enter b: "
     n3: .asciiz "Enter c: "
-    n3: .asciiz "Enter d: "
-    n4: .asciiz "Enter x: "
-    result: .asciiz "f = a.x^3 -b.x^2 + c.x -d "
+    n4: .asciiz "Enter d: "
+    n5: .asciiz "Enter x: "
+    result: .asciiz "f = a.x^3 - b.x^2 + c.x - d = "
 
 .text
         #getting first input.
@@ -27,9 +27,16 @@
     syscall
     li $v0, 5
     syscall
-    move $s3, $v0
+    move $s2, $v0
     #getting fourth input.
     la $a0, n4
+    li $v0, 4
+    syscall
+    li $v0, 5
+    syscall
+    move $s3, $v0
+    #getting fourth input.
+    la $a0, n5
     li $v0, 4
     syscall
     li $v0, 5
@@ -39,9 +46,15 @@
     la $a0, result
     li $v0, 4
     syscall
-    sub $t3, $s0, $s1
-    add $s4, $t3,$s3
-    move $a0, $s4
+    
+    mul $t0, $s0, $s4
+    sub $t0, $t0, $s1
+    mul $t0, $t0, $s4
+    add $t0, $t0, $s2
+    mul $t0, $t0, $s4
+    sub $t0, $t0, $s3
+    move $s0, $t0
+    move $a0, $s0
     li $v0, 1
     syscall
 
